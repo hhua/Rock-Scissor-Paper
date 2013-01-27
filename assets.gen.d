1005653e89c24d17bdc8d475e621d25f752d5b66
chinesecold <chinesecold@gmail.com>
2013-01-27 16:07:27 -0500

    connection

---
+++ assets.gen.d
@@ -0,0 +1,55 @@
+assets.gen.o: assets.gen.cpp \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/group.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/syscall.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/types.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/audio.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/events.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/vram.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/asset.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/elf.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/limits.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/macros.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/math.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/loader.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/array.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/memory.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/audio.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/image.h
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/group.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/syscall.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/types.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/audio.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/events.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/vram.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/asset.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/elf.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/limits.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/macros.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/math.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/loader.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/array.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/memory.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/audio.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/image.h:
---
+++ main.d
@@ -0,0 +1,115 @@
+main.o: main.cpp \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/syscall.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/types.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/audio.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/events.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/vram.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/asset.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/elf.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/array.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/memory.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/macros.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/group.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/limits.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/math.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/loader.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/audio.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/image.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/audio.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/cube.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/event.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/filesystem.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/metadata.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/motion.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/string.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/system.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/time.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/color.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/sprite.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/framebuffer.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/bg0rom.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/bg0.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/bg1.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/bg2.h \
+  /Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/tilebuffer.h \
+  assets.gen.h
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/syscall.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/types.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/audio.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/events.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/vram.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/asset.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/abi/elf.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/array.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/memory.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/macros.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/group.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/limits.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/math.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/loader.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/audio.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/asset/image.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/audio.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/cube.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/event.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/filesystem.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/metadata.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/motion.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/string.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/system.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/time.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/color.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/sprite.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/framebuffer.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/bg0rom.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/bg0.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/bg1.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/bg2.h:
+
+/Users/chinesecold/sifteo-sdk-mac64-v0.9.8-203-gc80efac/include/sifteo/video/tilebuffer.h:
+
+assets.gen.h:
